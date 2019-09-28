023967ea983f70e7c1d8498e7ee0967d13b336fd
Akansha Shah <shahakansha23@gmail.com>
2019-09-28 15:10:17 -0700

    added getter setter

--- Book.java
+++ Book.java
@@ -3,5 +3,21 @@ public class Book{
 private int bookId;
 private String name;
 
+public void setBookId(Integer bid)
+{
+	this.bookId= bid;
+}
+public int getBookId()
+{
+	return bookId;
+}
+public void setName(String name)
+{
+	this.name= name;
+}
+public String getName()
+{
+	return name;
+}
 }
 
